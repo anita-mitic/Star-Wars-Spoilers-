ac03d0d9fb955b77674ac483e60eb6c0f1ad9879
anita-mitic <mitic_anita@yahoo.com>
2018-01-13 22:24:17 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$('.spoiler').on('click', 'button', function(event) {
+  //Show the spoiler text
+  $(this).prev().show();
+  //Hide the "Reveal Spoiler" button
+  $(this).hide();
+});
+
+//Create the "Reveal Spoiler" button
+const $button = $('<button>Reveal Spoiler</button>')
+//Append to web page
+$('.spoiler').append($button);
+                  
+//Hide the spoiler text
+$('.spoiler span').hide();
